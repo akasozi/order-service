805c81f6a344c9e2f77902e67e9dd54e9b3bace8
Abu Kasozi <abukasozi@gmail.com>
2024-03-19 16:29:35 +0300

    Added deployment pipeline

--- src/main/java/com/polarbookshop/orderservice/config/ClientProperties.java
+++ src/main/java/com/polarbookshop/orderservice/config/ClientProperties.java
@@ -2,10 +2,12 @@ package com.polarbookshop.orderservice.config;
 
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 import java.net.URI;
 
 @ConfigurationProperties(prefix = "polar")
+@RefreshScope
 public record ClientProperties(
         @NotNull
         URI catalogServiceUri
