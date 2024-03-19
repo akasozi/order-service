8a9cde6970fa03fe7311eac3b5ac26f848f4d13e
Abu Kasozi <abukasozi@gmail.com>
2024-03-19 15:32:13 +0300

    Added Order Controller Tests

---
+++ src/test/java/com/polarbookshop/orderservice/order/domain/OrderRepositoryR2dbcTests.java
@@ -0,0 +1,50 @@
+package com.polarbookshop.orderservice.order.domain;
+
+import com.polarbookshop.orderservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DataR2dbcTest
+@Import(DataConfig.class)
+@Testcontainers
+class OrderRepositoryR2dbcTests {
+
+    static PostgreSQLContainer<?> postgresql =  new PostgreSQLContainer<>(DockerImageName.parse("postgres:14.4"));
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", OrderRepositoryR2dbcTests::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgresql::getUsername);
+        registry.add("spring.r2dbc.password", postgresql::getPassword);
+        registry.add("spring.flyway.url", postgresql::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s",
+                postgresql.getContainerIpAddress(),
+                postgresql.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT),
+                postgresql.getDatabaseName());
+    }
+
+    @Test
+    void createRejectedOrder() {
+        var rejectedOrder = OrderService.buildRejectedOrder("1234567890", 3);
+        StepVerifier.create(orderRepository.save(rejectedOrder))
+                .expectNextMatches(
+                        order -> order.status().equals(OrderStatus.REJECTED))
+                .verifyComplete();
+    }
+}
